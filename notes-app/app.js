6768931a44ed5015c8788ab1456fea37939675a4
Theo Weller <weller.theodor@gmail.com>
2020-02-13 14:35:54 -0500

    added add, remove, list, and read commands tonotes app

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,5 +1,48 @@
-const getNotes = require('./getNotes.js')
-console.log(getNotes('Who let the dogs out?'))
+//const validator = require('validator')
+const getNotes = require('./getNotes.js');
+
+const chalk = require('chalk');
+const yargs = require('yargs')
+ 
+//Create 'add' comand
+yargs.command({
+    command: 'add',
+    describe: 'Add new note...',
+    handler: function(){
+        console.log('Adding new note..')
+    }
+}).argv
+
+//Create 'remove' command
+yargs.command({
+    command: 'remove',
+    describe: 'Remove note...',
+    handler: function () {
+        console.log('Removing note...')
+    }
+}).argv
+
+//Create 'read' command
+yargs.command({
+    command: 'read',
+    describe: 'Reading note...',
+    handler: function(){
+        console.log('Reading note')
+    }
+}).argv
+
+//Create 'list' command
+yargs.command({
+    command: 'list',
+    describe: 'Listing notes...',
+    handler: function(){
+        console.log('Listing notes')
+    }
+}).argv
+
+// console.log(yargs.argv)
+
+//console.log(getNotes('Who let the dogs out?'))
 
 
 
@@ -8,3 +51,12 @@ console.log(getNotes('Who let the dogs out?'))
 //const fs = require('fs');
 //fs.writeFileSync('notes.txt', 'TEST!');
 //fs.appendFileSync('notes.txt', ' Append test.')
+
+
+/****************** Chalk + Examples ******************/
+// const chalk = require('chalk');
+// const log = console.log;
+// // Compose multiple styles using the chainable API
+// log(chalk.blue.bgRed.bold('Hello world!'));
+// // Combine styled and normal strings
+// log(chalk.blue('Hello') + ' World' + chalk.red('!'));
