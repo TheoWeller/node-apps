9d5aa5f9b8daa7cec0b29850cae530b7d8e699e8
Theo Weller <weller.theodor@gmail.com>
2020-02-13 13:30:46 -0500

    export practice

---
+++ notes-app/app.js
@@ -0,0 +1,7 @@
+const add = require('./utils.js')
+
+console.log(add(419, 1))
+
+//const fs = require('fs');
+//fs.writeFileSync('notes.txt', 'TEST!');
+//fs.appendFileSync('notes.txt', ' Append test.')
---
+++ notes-app/utils.js
@@ -0,0 +1,7 @@
+
+const name = 'Jershumpus Pebblerot'
+
+const add = function(a, b) {return a + b}
+
+//exports variiable - return output when file is required in other file
+module.exports = add
